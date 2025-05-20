fdd6cb930dbeae4d7253fac55f74bcdb8ec26a3e
Magloire Mr. KITIO <magloirekitio1@gmail.com>
2025-05-20 15:09:37 +0100

    solving messaging

--- findam/settings.py
+++ findam/settings.py
@@ -130,7 +130,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
-    'BLACKLIST_AFTER_ROTATION': True,
+    'BLACKLIST_AFTER_ROTATION': False,
 }
 
 # CORS settings
